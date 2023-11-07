2de6c7d44601448bff0bda59ceaa32c0358f6b47
Barbara Bothe <barbara.bothe@fau.de>
2023-11-07 15:16:53 +0100

    PDF

--- includes/PDF.php
+++ includes/PDF.php
@@ -12,10 +12,10 @@ class PDF {
     public static function printPDF($services) {
         if (!isset($_GET['services']))
             return;
-        $logo = plugin()->getPath() . 'assets/img/logo_fau.svg';
         $services = explode(',', sanitize_text_field($_GET['services']));
 
-        $pdf = new TCPDF();
+        $pdf = new RRZE_PDF();
+
         // set document information
         $pdf->SetCreator(PDF_CREATOR);
         $pdf->SetAuthor(get_bloginfo('name'));
@@ -24,83 +24,116 @@ class PDF {
         $pdf->SetKeywords('RRZE, Services, Dienstleistungen, FAU');
 
         // set margins
-        $pdf->SetMargins(15, 15, 15, true);
+        $pdf->SetMargins(15, 40, 15, true);
 
         // set auto page breaks
-        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-        // remove default header/footer
-        $pdf->setPrintHeader(false);
-        $pdf->setPrintFooter(false);
+        $pdf->SetAutoPageBreak(TRUE, 30);
 
         // Set font
         $pdf->SetFont('helvetica', '', 10, '', true);
 
         // Add a page
-        // This method has several options, check the source code documentation for more information.
-        $pdf->AddPage();
+        $pdf->AddPage('P', '', true);
 
-        // Add logo
-        $pdf->SetX(12.5);
-        $pdf->ImageSVG($logo, null, 0, 50, 30, '', 'N', '');
-        $pdf->setXY(100, 6);
-        $pdf->setFontSpacing(0.1);
-        $pdf->SetTextColor(4,49,106);
-        $pdf->MultiCell(90, 30, 'Friedrich-Alexander-Universität<br />Regionales Rechenzentrum Erlangen', 0, 'R', false, 1, null, null, true, 0, true);
-        $pdf->setFontSpacing(0);
+        // Write Title
+        $pdf->SetTextColor(40,60,122);
+        $pdf->WriteHTMLCell(0, 5, null, null, '<h1 style="font-weight: normal; font-size: 24px;">' . __('Your RRZE Service Catalogue', 'rrze-servicekatalog') . '</h1>', 0, 1);
+
+        $pdf->SetY($pdf->getY() + 5);
 
         // Write Content
-        $pdf->SetX(15);
-        $pdf->SetTextColor(4,49,106);
-        $pdf->SetFontSize(20);
-        $pdf->MultiCell(0,5, __('RRZE Servicekatalog', 'rrze-servicekatalog'), 0, 'L');
+        $pdf->setEqualColumns(2, 80);
 
         foreach ($services as $id) {
             $meta = get_post_meta($id);
-            $links = [];
-            $links['portal']['label'] = __('Portal', 'rrze-servicekatalog');
-            $links['portal']['url'] = Utils::getMeta($meta, 'url-portal');
-            $links['description']['label'] = __('Service Description', 'rrze-servicekatalog');
-            $links['description']['url'] = Utils::getMeta($meta, 'url-description');
-            $links['tutorial']['label'] = __('Tutorial', 'rrze-servicekatalog');
-            $links['tutorial']['url'] = Utils::getMeta($meta, 'url-tutorial');
-            $links['video']['label'] = __('Video Tutorial', 'rrze-servicekatalog');
-            $links['video']['url'] = Utils::getMeta($meta, 'url-video');
 
+            // Title
+            $pdf->SetTextColor(40, 60, 122);
+            $pdf->WriteHTMLCell(0, 0, NULL, NULL, '<h2 style="font-size: 18px;">' . get_the_title($id) . '</h2>', 0, 1);
+
+            // Description
+            $pdf->SetY($pdf->getY() + 1);
+            $pdf->SetTextColor(0, 0, 0);
+            $pdf->SetFontSize(10);
+            $pdf->WriteHTMLCell(0, 0, NULL, NULL, Utils::getMeta($meta, 'description'), 0, 1);
+
+            // Commitment
             $commitmentTerms = get_the_terms($id, 'rrze-service-commitment');
             if ($commitmentTerms) {
                 $commitmentName = $commitmentTerms[0]->name;
                 $commitmentColor = get_term_meta($commitmentTerms[0]->term_id, 'rrze-service-commitment-color', TRUE);
-            }
-
-            $pdf->SetTextColor(4,49,106);
-            $pdf->SetFontSize(16);
-            $pdf->SetY($pdf->getY() + $pdf->getFontSize());
-            $pdf->MultiCell(0,0, get_the_title($id), 0, 'L');
-
-            $pdf->SetTextColor(0,0,0);
-            $pdf->SetFontSize(10);
-            $pdf->SetY($pdf->getY() + $pdf->getFontSize() / 2);
-            $pdf->MultiCell(0,5, Utils::getMeta($meta, 'description'), 0, 'L', false, 1, null, null, true, 0, true);
-
-            if ($commitmentTerms) {
-                $pdf->SetY($pdf->getY() + $pdf->getFontSize() / 2);
-                $pdf->MultiCell(0,5, __('Use', 'rrze-servicekatalog') . ': ' . $commitmentName, 0, 'L', false, 1, null, null, true, 0, true);
-            }
-
-            $pdf->SetY($pdf->getY() + $pdf->getFontSize() / 2);
-            foreach ($links as $link) {
-                if ($link['url'] != '') {
-                    $pdf->MultiCell(0,5, $link['label'] . ': <a href="' . $link['url'] . '">' . $link['url'] . '</a>', 0, 'L', false, 1, null, null, true, 0, true);
+                if ($commitmentColor == '') {
+                    $commitmentColor = '#ffffff';
                 }
+                // hex2rgb
+                (strlen($commitmentColor) === 4) ? list($commitmentColorR, $commitmentColorG, $commitmentColorB) = sscanf($commitmentColor, "#%1x%1x%1x") : list($commitmentColorR, $commitmentColorG, $commitmentColorB) = sscanf($commitmentColor, "#%2x%2x%2x");
+
+                $pdf->SetY($pdf->getY() + $pdf->getFontSize() / 2, FALSE);
+                $pdf->Rect($pdf->getX(), $pdf->getY() + 0.25, 2.5, 4, 'DF', ['all' => ['width' => 1, 'color' => ['255', '255', '255']]], [$commitmentColorR, $commitmentColorG, $commitmentColorB]);
+                $pdf->MultiCell(0, 0, __('Use', 'rrze-servicekatalog') . ': ' . $commitmentName, 0, 'L', FALSE, 1, $pdf->getX() + 2, NULL);
+                $pdf->SetY($pdf->GetY() + 2);
             }
+
+            // QR Code Service Description
+            $nextY = $pdf->getY();
+            $urlDescription = esc_url(Utils::getMeta($meta, 'url-description'));
+            if ( ! empty($urlDescription)) {
+                $style = [
+                    'border' => FALSE,
+                    'hpadding' => 1,
+                    'bgcolor' => FALSE
+                ];
+                $nextY = $pdf->getY() + 30;
+                $pdf->write2DBarcode(Utils::getMeta($meta, 'url-description'), 'QRCODE,H', NULL, NULL, 22, 20, $style, 'T');
+            }
+
+            // URL Service Description
+            if ( ! empty($urlDescription)) {
+                $pdf->WriteHTMLCell(0, 5, NULL, NULL, '<a href="' . $urlDescription . '" style="color: #004a9f;">' . $urlDescription . '</a>', 0, 1);
+            }
+
+            $pdf->SetY($nextY);
         }
 
-        // ---------------------------------------------------------
+        $pdf->ResetColumns();
 
         // Close and output PDF document
-        // This method has several options, check the source code documentation for more information.
         $pdf->Output('servicekatalog_' . date('Y-m-d') . '.pdf', 'I');
         exit;
+    }
+}
+
+class RRZE_PDF extends TCPDF{
+    //Page header
+    public function Header() {
+        $logoFAU = plugin()->getPath() . 'assets/img/logo_fau.svg';
+        $this->SetX(12.5);
+        $this->ImageSVG($logoFAU, null, 5, 50, 30, '', 'N', '');
+        $this->setXY(130, 11);
+        $this->SetFont('helvetica', '', 10, '', true);
+        $this->setFontSpacing(0.1);
+        $this->SetTextColor(40,60,122);
+        $this->WriteHTMLCell(65, 30, null, null, 'Friedrich-Alexander-Universität<br />Regionales Rechenzentrum Erlangen', 0, 1);
+        $this->setFontSpacing(0);
+    }
+
+    // Page footer
+    public function Footer() {
+        // Position at 15 mm from bottom
+        $this->SetY(-25);
+        // Set font
+        $this->SetFont('helvetica', 'I', 8);
+        // Page number
+        //$this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+
+        $this->SetTextColor(40,60,122);
+        $this->SetFontSize(8);
+        $this->MultiCell(110, 18, "Regionales Rechenzentrum Erlangen (RRZE) | Martensstraße 1 | 91058 Erlangen \nStand: " . date('M Y'), 0, 'L', false, 0, null, null, true, 0, false, true, 18, 'B');
+        //$this->Cell(110, 5, "Regionales Rechenzentrum Erlangen (RRZE) | Martensstraße 1 | 91058 Erlangen", 0, 1, 'L', false, '', 0, true, 'T', 'B');
+        //$this->Cell(110, 5, "Stand: " . date('F Y'), 0, 1, 'L', false, '', 0, true, 'T', 'B');
+        $this->SetX(155);
+        // $this->SetY(268, false);
+        $logoRRZE = plugin()->getPath() . 'assets/img/logo_rrze.svg';
+        $this->ImageSVG($logoRRZE, '', '', 40, 18, '', 'T', 'R');
     }
 }
