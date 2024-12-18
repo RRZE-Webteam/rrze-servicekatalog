3f87771ef2ab3bb4235f76181791d2cb02f5e1ad
Barbara Bothe <barbara.bothe@fau.de>
2024-12-18 14:33:41 +0100

    TCPDF Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'tecnickcom/tcpdf' => array(
-            'pretty_version' => '6.7.5',
-            'version' => '6.7.5.0',
-            'reference' => '951eabf0338ec2522bd0d5d9c79b08a3a3d36b36',
+            'pretty_version' => '6.7.8',
+            'version' => '6.7.8.0',
+            'reference' => '7956f5e37863c6a569d5ccfae826f353a12a2493',
             'type' => 'library',
             'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
             'aliases' => array(),
