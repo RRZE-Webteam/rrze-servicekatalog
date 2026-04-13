c190f0341b357ca9140db45916b6a57f6816db6f
Barbara Bothe <barbara.bothe@fau.de>
2026-04-13 11:25:45 +0200

    Bugfix fehlendes JS, Update dependencies
    fixes #40

--- build/block/index.asset.php
+++ build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6586cc1dd0d163a99e48');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '50f46c39df4266d35680');
